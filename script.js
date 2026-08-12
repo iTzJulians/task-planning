502e676b6647f9a217658db12bf326cf2ce7b66f
iTzJulians <itzjulian14@gmail.com>
2026-08-12 14:32:54 -0500

    design css

---
+++ script.js
@@ -0,0 +1,11 @@
+const modal = document.getElementById("modal");
+const closeModal = document.getElementById("closeModal");
+const addTaskButton = document.getElementById("addTask");
+addTaskButton.addEventListener("click", ()=> {
+    modal.classList.remove("d-none")
+
+})
+closeModal.addEventListener("click", ()=> {
+modal.classList.add("d-none")
+
+});
